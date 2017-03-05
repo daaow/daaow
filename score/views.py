5cbccbf2a5f014e66e0347cd97a630d7fc60a6eb
Baymax Chuang <350535629@qq.com>
2017-03-05 22:40:59 +0800

    Upload the index.html but haven't add the url.

--- score/views.py
+++ score/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
-
+from django.http import HttpResponse
+from django.template import loader
+def index(request):
+    template = loader.get_template('score/index.html')
+    return HttpResponse(template.render(ikk,request))
 # Create your views here.
