f3333b410cf9ea97c77fec4c077371d14c90186d
x1ah <x1ahgxq@gmail.com>
2017-03-10 17:17:58 +0800

    完成首页登录入口 完成所有功能

--- score/views.py
+++ score/views.py
@@ -26,13 +26,10 @@ def ulogin(request):
                 request.session['info'] = info_to_json(stu.get_info())
                 score_pre = stu.get_score()
                 score_pre['lessons'].sort(key=lambda x: x[1], reverse=True)
-                # TODO:
-                # - score API 挂科记录接口待添加，暂时设置为False
-                # - CET-4, CET-6 接口
                 request.session['score'] = score_pre
                 request.session['login'] = True
                 request.session['npass'] = stu.get_npass_lesson()
-                # request.session['elec'] = stu.get_elec # 选修课
+                request.session['elec'] = stu.get_elective() # 选修课
 
                 messages.success(request, '登录成功')
                 return render(request, 'score/index.html',
@@ -52,13 +49,16 @@ def ulogin(request):
             tags = {
                 'npass': request.session.get('npass').get('npass'),
                 'cet': list(filter(lambda x: '等级考试' in x, lessons)),
-                'default':lessons
+                'elec': request.session.get('elec').get('lessons'),
+                'default':list(filter(lambda x: '等级考试' not in x, lessons))
             }
             return render(
                 request,
                 'score/index.html',
                 {
                     'lessons': tags.get(tag, lessons),
+                    'elec': tag == 'elec',
+                    'cet': tag =='cet',
                 }
             )
         else:
@@ -68,7 +68,7 @@ def ulogin(request):
 def ulogout(request):
     request.session.flush()
     messages.success(request, "已登出")
-    return HttpResponseRedirect('/score/')
+    return HttpResponseRedirect('/')
 
 def about(request):
     return render(request, 'score/about.html')
